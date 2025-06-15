90cadbe7a9db1609189dd1671ddb82066aacb658
Maruca Cantu Valdes <89872076+A00834245@users.noreply.github.com>
2025-06-14 19:18:19 -0600

    Login

--- dimeloc/MapView.swift
+++ dimeloc/MapView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
+import MapKit
 
 struct MapView: View {
-    var body: some View {
-        Text("Map")
-            .font(.title)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    }
+    @State private var region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 25.6866, longitude: -100.3161), // Monterrey
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+
+        var body: some View {
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all)
+        }
 }
+
+
 
 #Preview {
     MapView()
---
+++ dimeloc/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  dimeloc
+//
+//  Created by DLmac on 14/06/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+            .font(.title)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
